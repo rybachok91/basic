3219af9ab97edec0fdbbf0a061d11483459a90dc
Rybachek Tatiana <t.rybachek@newcontact.su>
2018-04-19 11:27:03 +0300

    убрала диаграмму

--- views/site/index.php
+++ views/site/index.php
@@ -4,47 +4,7 @@
 
 $this->title = 'My Yii Application';
 ?>
-<?php
-$this->registerJsFile('https://www.gstatic.com/charts/loader.js');
-$this->registerJs(
-      "google.charts.load('current', {'packages':['corechart']});
-
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawChart);
-
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawChart() {
-
-      // Create the data table.
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Название');
-      data.addColumn('number', 'Количество');
-      data.addRows([
-        ['Mushrooms', 4],
-        ['Onions', 1],
-        ['Olives', 1], 
-        ['Zucchini', 1],
-        ['Pepperoni', 2]
-      ]);
-
-      // Set chart options
-      var options = {'title':'Сколько пиццы съели',
-                     'width':500,
-                     'height':300};
-
-      // Instantiate and draw our chart, passing in some options.
-      var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
-    }"
-);
-?>
-
 
 <div class="site-index">
-    <div class="jumbotron">
-        <!--Div that will hold the pie chart-->
-        <div id="chart_div" style="width:400; height:300"></div>
-    </div>
+
 </div>
