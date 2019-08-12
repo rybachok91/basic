ceb97e13a84c3089a4cdb4927884643a4422e9ca
Rybachek <t.rybachek@newcontact.su>
2019-08-12 15:25:12 +0300

    login 1

--- models/User.php
+++ models/User.php
@@ -126,4 +126,13 @@ class User extends Model implements IdentityInterface
         return true;
     }
 
+    /**
+     * Logs in a user using the provided username and password.
+     * @return bool whether the user is logged in successfully
+     */
+    public function login3()
+    {
+        return true;
+    }
+
 }
